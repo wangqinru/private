397dd27cfe1a1fc2259bde4ba5acd1e24039243b
王 秦如 <oshinjyo14@gmail.com>
2019-03-21 02:50:13 +0900

    プロジェクト上げなおし

---
+++ Assets/Scripts/DamageManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageManager : MonoBehaviour
+{
+    [SerializeField]
+    RectTransform canvas;
+
+    [SerializeField]
+    GameObject damageObject;
+
+    [SerializeField]
+    Sprite[] damageTextures;
+
+    [SerializeField]
+    int damage = 34512;
+
+    private void Start()
+    {
+        CreateDamage(damage, new Vector2(50.0f, 50.0f));
+    }
+
+    void CreateDamage(int dmg, Vector2 pos)
+    {
+        GameObject obj = Instantiate(damageObject);
+
+        obj.transform.SetParent(canvas, false);
+        obj.transform.localPosition = pos;
+
+        DamageSample dmgSctipt = obj.GetComponent<DamageSample>();
+
+        dmgSctipt.SetDamage(dmg, damageTextures);
+    }
+}
---
+++ Assets/Scripts/DamageSample.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageSample : MonoBehaviour
+{
+    [SerializeField]
+    Image[] damageImages;
+
+    public void SetDamage(int dmg, Sprite[] damageTextures)
+    {
+        string damageText = dmg.ToString();
+
+        if (damageText.Length > damageImages.Length) { return; }
+
+        for (int i = 0; i < damageImages.Length; i++)
+        {
+            if (i >= damageText.Length)
+            {
+                damageImages[i].gameObject.SetActive(false);
+                continue;
+            }
+
+            int num = int.Parse(damageText.Substring(i, 1));
+            if (num >= damageTextures.Length) { return; }
+
+            if (i >= damageImages.Length) { return; }
+            damageImages[i].sprite = damageTextures[num];
+
+            damageImages[i].gameObject.SetActive(true);
+        }
+    }
+
+}
